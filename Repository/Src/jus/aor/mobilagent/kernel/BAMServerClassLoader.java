4129f807d89f879f69f1b4651f1e78dd1853c4e2
DESKTOP-GIB7BNL\Djypzi <antoineblanc269@laposte.net>
2017-03-31 10:45:29 +0200

    ajout de la class BAMServerClassLoader

---
+++ Repository/Src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
@@ -0,0 +1,13 @@
+package jus.aor.mobilagent.kernel;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class BAMServerClassLoader extends URLClassLoader{
+
+	public BAMServerClassLoader(URL[] urls, ClassLoader parent) {
+		super(urls, parent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
