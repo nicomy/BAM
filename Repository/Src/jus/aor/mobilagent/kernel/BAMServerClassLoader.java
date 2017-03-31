40d3722798e2d5c69e8ed451f765d2dce5d46e15
DESKTOP-GIB7BNL\Djypzi <antoineblanc269@laposte.net>
2017-03-31 11:20:20 +0200

    ajout methode class BAMServerClassLoader

--- Repository/Src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
+++ Repository/Src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
@@ -9,5 +9,19 @@ public class BAMServerClassLoader extends URLClassLoader{
 		super(urls, parent);
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+	protected void addURL(URL url) {
+		// TODO Auto-generated method stub
+		super.addURL(url);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+
 
 }
